3314b5677ec042f39dad1623be7f5b542d36a417
David Flemström <david.flemstrom@gmail.com>
2019-05-20 19:22:33 +0200

    Start match-keyboard in user mode

--- chromium_kiosk/deploy.sh
+++ chromium_kiosk/deploy.sh
@@ -1,4 +1,6 @@
-docker run --rm --privileged -v \
-    ~/.docker:/root/.docker homeassistant/amd64-builder \
-    --all -t chromium_kiosk -r https://github.com/dflemstr/hassio-addons
-
+docker run --rm --privileged \
+  -v ~/.docker:/root/.docker \
+  -v $PWD:/data \
+  homeassistant/amd64-builder \
+  --all \
+  -t /data
