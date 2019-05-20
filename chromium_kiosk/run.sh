1d564618a231e808bbcdff0c17d3fe46006b80db
David Flemström <david.flemstrom@gmail.com>
2019-05-20 19:37:28 +0200

    Temporarily disable keyboard

--- chromium_kiosk/run.sh
+++ chromium_kiosk/run.sh
@@ -8,7 +8,7 @@ config_path=/data/options.json
 url="$(jq --raw-output '.url' "$config_path")"
 
 echo "#!/bin/sh" > /usr/local/bin/start
-echo "/usr/bin/matchbox-keyboard --daemon" >> /usr/local/bin/start
+#echo "/usr/bin/matchbox-keyboard --daemon" >> /usr/local/bin/start
 echo "/usr/bin/chromium-browser --kiosk '$url' --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage" >> /usr/local/bin/start
 /usr/bin/dbus-daemon --system
 su -c 'startx /usr/local/bin/start' chromium
